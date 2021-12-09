4e81ca32a509a91fe589ebf634da7bbe30c834cd
anonymous anonymous <liatgwerber@gmail.com>
2021-12-09 00:01:18 +0000

    Whitelist columns in Course for ActiveAdmin

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Course do
 
+ permit_params :description, :professor_id, :course_name, :quarter, :course_time
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
