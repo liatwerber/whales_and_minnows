13cfb24a6ee72878fce71ae6bf2020e5016831fc
anonymous anonymous <liatgwerber@gmail.com>
2021-12-09 00:00:55 +0000

    Validate uniqueness of bookmarks.course_id

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,6 +5,8 @@ class Bookmark < ApplicationRecord
 
   # Validations
 
+  validates :course_id, :uniqueness => { :scope => [:user_id], :message => "already liked" }
+
   validates :course_id, :presence => true
 
   validates :user_id, :presence => true
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Bookmark, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:course_id).scoped_to(:user_id).with_message('already liked') }
+
     it { should validate_presence_of(:course_id) }
 
     it { should validate_presence_of(:user_id) }
